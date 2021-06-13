2e75f0d152314acd41d1ad30e0d60dc5d34ea89f
Tábata Gonçalves de Carvalho <tabatagc@gmail.com>
2021-06-13 12:45:43 +0100

    Create Account.cs

---
+++ Account.cs
@@ -0,0 +1,26 @@
+namespace TestDome
+{
+    using System;
+
+    public class Account
+    {
+        [Flags]
+        public enum Access
+        {
+            Delete = 1,
+            Publish = 2,
+            Submit = 4,
+            Comment = 8,
+            Modify = 16,
+
+            Writer = Submit | Modify,
+            Editor = Delete | Publish | Comment,
+            Owner = Writer | Editor
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(Access.Writer.HasFlag(Access.Delete)); //Should print: "False"
+        }
+    }
+}
